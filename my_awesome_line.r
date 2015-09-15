cba785ab397a3b8fb5ff0f2303b03d5006d9ebe6
RashedHUBC <rhoque@isrt.ac.bd>
2015-09-15 11:05:11 -0700

    add a new file.

---
+++ my_awesome_line.r
@@ -0,0 +1,24 @@
+a <- 2
+
+b <- 3
+
+sig_sq <- 1
+
+x <- runif(40)
+
+y <- a + b*x + rnorm(40, sd= sqrt(sig_sq))
+
+(avg_x <- mean(x))
+
+write(avg_x, "avg_x.txt")
+
+plot(x,y)
+
+abline(a=a, b=b, col="hotpink", lwd=2)
+
+my_fit <- lm(y~x)
+
+####hello...
+
+summary(my_fit)
+
